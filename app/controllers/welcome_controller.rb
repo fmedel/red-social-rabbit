513b9a80b0677455f953e4af81711a343d326841
fmedel <felipemedelatorre@hotmail.com>
2016-04-14 21:11:26 -0300

    index p1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @title = "Red Rabbit Network"
+    @footer = "<h1>hoojh<h1>"
+  end
+  def token_user
+    
   end
 end
--- app/models/denuncia.rb
+++
@@ -1,2 +0,0 @@
-class Denuncia < ActiveRecord::Base
-end
