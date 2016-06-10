4d82d6dee4897d7611b9476b30036efa25bf2125
fmedel <felipemedelatorre@hotmail.com>
2016-06-09 23:21:13 -0400

    nada

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def create
-
+ 
   	palabra = "%#{params[:keyword]}%"
 
   	@users = User.where("nombre_empresa LIKE ?", palabra )
@@ -15,7 +15,7 @@ class SearchController < ApplicationController
     
     palabra = "%#{params[:keyword]}%"
 
-    @ideas = Idea.where("titulo LIKE ?", palabra ) #hay que cambiar
+    @ideas = Idea.select('"denuncia".*,"ideas".id as "id ideas","ideas".titulo , "ideas".contenido, "ideas".visita, "ideas".created_at as "created_at idea", "ideas".updated_at as "updated_at ideas", "ideas".user_id').joins("FULL OUTER JOIN denuncia  ON  ideas.id= denuncia.idea_id").where("titulo LIKE ? AND ideas.estado_id IN (3, 5)", palabra ) #hay que cambiar
 
     respond_to do |format|
       format.html { redirect_to root_path }
@@ -25,3 +25,4 @@ class SearchController < ApplicationController
 
   end
 end
+ 
